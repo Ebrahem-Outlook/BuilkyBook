8c7114324fa52d5ce4498549d18c5488678cdd04
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-29 13:06:00 +0200

    V_3 Veiw

--- BuilkyBookWeb/Controllers/CategoryController.cs
+++ BuilkyBookWeb/Controllers/CategoryController.cs
@@ -10,10 +10,17 @@ namespace BuilkyBookWeb.Controllers
         {
             _db = db;
         }
+
         public IActionResult Index()
         {
             IEnumerable<Category> ObjCategories = _db.categories.ToList();
             return View(ObjCategories);
+        }
+
+        // Get
+        public IActionResult create()
+        {
+            return View();
         }
     }
 }
--- BuilkyBookWeb/Program.cs
+++ BuilkyBookWeb/Program.cs
@@ -1,5 +1,6 @@
 using BuilkyBookWeb.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // DI
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection")));
+
 
 var app = builder.Build();
 
