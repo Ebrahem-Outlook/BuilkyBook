1c2f1357a228f9f48c1ba1e2497ed075c661f00a
EbrahemElbetar1 <132098790+EbrahemElbetar1@users.noreply.github.com>
2024-01-29 02:56:39 +0200

    V_2 Controller Not Finish

---
+++ BuilkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BuilkyBookWeb.Data;
+using BuilkyBookWeb.Models;
+namespace BuilkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> ObjCategories = _db.categories.ToList();
+            return View(ObjCategories);
+        }
+    }
+}
